2df89261914472145224ab4242e0ed3fb0b3b02c
Spidey <setora087@gmail.com>
2023-11-19 13:41:36 +0500

    Temporarily ended header making

--- frontend/src/components/FirstNavbar.jsx
+++ frontend/src/components/FirstNavbar.jsx
@@ -1,19 +1,20 @@
 function FirstNavbar() {
     return (
         <nav className="w-full bg-transparent  flex items-center justify-center">
+            <img src="/Small-removebg-preview.png" alt="" className='w-[5%]'/>
             <div className="w-[80%] flex items-center justify-between py-8">
                 <div className="flex gap-4 justify-between">
                     <a href="#"
-                       className="block py-2 px-3 text-white text-xl md:hover:text-blue-600">For employers</a>
+                       className="block py-2 px-3 text-white text-xl hover:text-blue-600 md:hover:text-blue-600">For employers</a>
                     <a href="#"
-                       className="block py-2 px-3 text-white text-xl md:hover:text-blue-600">Help</a>
+                       className="block py-2 px-3 text-white text-xl hover:text-blue-600 md:hover:text-blue-600">Help</a>
                 </div>
                 <div className='flex gap-4 justify-between'>
                     <button
-                        className="text-green-400 border-green-900 rounded-full hover:bg-gray-100 md:hover:bg-transparent md:hover:border-green-400 md:border md:p-1 md:px-4 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent text-xl">Create Summary
+                        className="px-4 py-1 text-green-400 border-green-900 rounded-full border md:hover:bg-transparent hover:border-green-400 md:hover:border-green-400 md:border md:p-1 md:px-4 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent text-xl">Create Summary
                     </button>
                     <button
-                        className="text-white border-gray-500 rounded-full hover:bg-gray-100 md:hover:bg-transparent md:hover:border-white md:border md:p-1 md:px-4 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent text-xl">Log in
+                        className="px-4 py-1 text-white border-gray-500 rounded-full border hover:border-white md:hover:bg-transparent md:hover:border-white md:border md:p-1 md:px-4 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent text-xl">Log in
                     </button>
                 </div>
             </div>
--- frontend/src/pages/GetStarted.jsx
+++ frontend/src/pages/GetStarted.jsx
@@ -9,9 +9,12 @@ function GetStarted() {
                 <div className="w-full h-[60vh] flex justify-center items-center">
                     <button className='bg-green-500 rounded text-white px-10 py-5'>GET STARTED</button>
                 </div>
-                <div>
-                    <h1 className='text-white'>
-                        676156 <br/> Summaries
+                <div className='flex'>
+                    <h1 className='text-white text-2xl ml-[100px] font-semibold'>
+                        676 156 <br/> Summaries
+                    </h1>
+                    <h1 className='text-white text-2xl ml-[50px] font-semibold'>
+                        781 354 <br/> Vacancies
                     </h1>
                 </div>
             </div>
